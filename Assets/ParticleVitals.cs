2027a39a60fd59cdac749dd22415d0304daaf264
Mark De Ruyter <mderu001@ucr.edu>
2018-02-24 01:07:15 -0800

    Lighting and Particle Effects
    These are WIPs

---
+++ Assets/ParticleVitals.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleVitals : MonoBehaviour
+{
+    [Range(0, 1)]
+    public float _health = 1.0f;
+
+    public float health
+    {
+        get { return _health; }
+        set
+        {
+            _health = value;
+            playerLight.intensity = _health;
+            //bodyParticles.main.startColor = Color.red;
+        }
+    }
+
+    [SerializeField]
+    private Light playerLight;
+
+    [SerializeField]
+    private ParticleSystem bodyParticles;
+
+    [SerializeField]
+    private ParticleSystem trailParticles;
+}
